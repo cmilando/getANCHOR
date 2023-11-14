3ca214f3a82b36a2b62599dab7fdb62e0e7ef3ff
Chad Milando <44262671+cmilando@users.noreply.github.com>
2023-11-14 00:13:04 -0500

    update

--- R/find_in_readmes.R
+++ R/find_in_readmes.R
@@ -21,11 +21,13 @@ get_all_readme_text <- function(all_readmes) {
 
 #' This search text
 #' 
-#' Obviously a lot to update here
+#' Obviously a lot to update here, but not a bad start
 #' 
 #' @param readme_text a list output from get_all_readme_text, probably could be a class
 #' @param search_string what you are looking for
 #' @return the subset of list elements with that text string
 find_search_text <- function(readme_text, search_string) {
-  
+  lapply(readme_text, function(y) {
+    y[which(grepl(x = tolower(y), pattern = tolower(search_string)))]
+  })
 }
