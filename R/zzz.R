0323b307252f46843af8fcc236730619177a43ef
Chad Milando <44262671+cmilando@users.noreply.github.com>
2023-11-13 21:41:02 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("getANCHOR updated")
+
 }
 
 .onLoad <- function(libname, pkgname) {
-  # this is probably an awful idea
-  devtools::install_github("cmilando/getANCHOR")
-  library(getANCHOR)
+
 }
